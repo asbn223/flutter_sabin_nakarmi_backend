0b3706eca353156eebd5d7b734a375e223a1e9f6
asbn223 <asbn2231@gmail.com>
2025-04-09 10:18:49 +0545

    New

--- backend/urls.py
+++ backend/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
@@ -33,3 +35,7 @@ urlpatterns = [
     path('api/', include(router.urls)),
     path('api/users/', include('user.urls')),
 ]
+
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
