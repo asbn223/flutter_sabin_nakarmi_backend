973ab9b2d7ec61a29d074260634da84f3f69a461
asbn223 <asbn2231@gmail.com>
2025-04-16 15:52:39 +0545

    Fix Issue to create order request

--- order/views.py
+++ order/views.py
@@ -1,5 +1,8 @@
 from knox.auth import TokenAuthentication
 from rest_framework import generics, permissions
+from rest_framework.response import Response
+
+from product.models import Product
 from .models import OrderRequest
 from .serializer import OrderRequestSerializer
 
@@ -19,5 +22,43 @@ class OrderRequestCreateView(generics.CreateAPIView):
     permission_classes = [permissions.IsAuthenticated]
     authentication_classes = (TokenAuthentication,)
 
-    def perform_create(self, serializer):
-        serializer.save(user=self.request.user)
+    def create(self, request, *args, **kwargs):
+        data = request.data
+        size = data.get('size')
+        color = data.get('color')
+        product_id = data.get('product')
+        user = request.user
+
+        try:
+            product_exists = Product.objects.filter(id=product_id).exists()
+
+            if product_exists:
+                product = Product.objects.get(id=product_id)
+
+                OrderRequest.objects.create(user=user, product=product, size=size, color=color)
+
+                return Response(
+                    data=
+                    {
+                        'message': 'Order request has been sent successfully',
+                    },
+                    status=201
+                )
+
+            else:
+                return Response(
+                    data=
+                    {
+                        'message': 'Product not found'
+                    },
+                    status=404
+                )
+
+        except Exception as e:
+            return Response(
+                data=
+                {
+                'message': 'Order request cannot be sent'
+            },
+                status=400
+            )
